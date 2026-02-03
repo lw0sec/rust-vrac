0ed67b5caa3bd0f80d67ceb27583bc24540f7d11
lw0sec <lw0sec@protonmail.com>
2026-02-03 12:07:32 +0100

    wrapped-allocator

---
+++ wrapped-allocator/src/main.rs
@@ -0,0 +1,28 @@
+use std::alloc::{System, GlobalAlloc, Layout};
+use std::sync::atomic::{AtomicUsize, Ordering::SeqCst};
+
+struct Counter;
+
+static ALLOCATED: AtomicUsize = AtomicUsize::new(0);
+
+unsafe impl GlobalAlloc for Counter {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let ret = System.alloc(layout);
+        if !ret.is_null() {
+            ALLOCATED.fetch_add(layout.size(), SeqCst);
+        }
+        ret
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        System.dealloc(ptr, layout);
+        ALLOCATED.fetch_sub(layout.size(), SeqCst);
+    }
+}
+
+#[global_allocator]
+static A: Counter = Counter;
+
+fn main() {
+    println!("allocated bytes before main: {}", ALLOCATED.load(SeqCst));
+}
